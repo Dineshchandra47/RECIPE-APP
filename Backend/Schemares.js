ead8015807106357c28ecc32818949e6dbae6d73
Dineshchandra47 <119965753+Dineshchandra47@users.noreply.github.com>
2023-02-25 18:51:30 +0530

    Delete Backend directory

--- Backend/Schemares.js
+++
@@ -1,8 +0,0 @@
-const mongoose =require('mongoose')
-
-const mod =new mongoose.Schema({
-    email:{type:String},
-    password:{type:String}
-})
-const modes =mongoose.model("Recipp",mod)
-module.exports=modes
